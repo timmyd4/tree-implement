60fc4258911a030fc530ea89f63a93ef8e8780e7
tim <timmywilliams4665@gmail.com>
2025-04-29 11:25:28 -0700

    just saving

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,11 @@
 public class Traversal {
   public static void main(String[] args) {
 
+    TreeNode root = new TreeNode(10, null, null);
+    TreeNode root2pointO = new TreeNode(15);
+
+
+    System.out.println(root.value);
+    System.out.println(root2pointO.value);
   }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,18 @@
 public class TreeNode {
+    int value;
+    TreeNode left;
+    TreeNode right;
 
+    public TreeNode(int value, TreeNode left, TreeNode right)
+    {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
   
+
+    public TreeNode(int value)
+    {
+        this(value, null, null);
+    }
 }
